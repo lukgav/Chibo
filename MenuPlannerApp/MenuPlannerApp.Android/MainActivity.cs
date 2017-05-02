771110f4bec37f9867a08d359daf7f80fc229d99
Jack Fuge <jack.fuge@gmail.com>
2017-05-02 22:42:43 +1000

    updated app name on android home screen. now correctly named + icon :smile:

--- MenuPlannerApp/MenuPlannerApp.Android/MainActivity.cs
+++ MenuPlannerApp/MenuPlannerApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Chibo.Droid
 {
-    [Activity(Label = "MenuPlannerApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Chibo", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
