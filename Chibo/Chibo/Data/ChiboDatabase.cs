7e2df37a6877fccdbbd6bfaf7f772092f8b435f3
Jack Fuge <jack.fuge@gmail.com>
2017-05-10 00:16:07 +1000

    add filehelper frameworks and base files.

---
+++ Chibo/Chibo/Data/ChiboDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Chibo.Data
+{
+    public class ChiboDatabase
+    {
+        readonly SQLiteAsyncConnection database;
+
+        public ChiboDatabase(string dbpath)
+        {
+            //the constructor for the database
+            database = new SQLiteAsyncConnection(dbpath);
+            //database.CreateTableAsync<>().Wait();
+        }
+    }
+}
