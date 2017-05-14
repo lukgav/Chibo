f5c174a2282a8baf598bf351044dd662d7fe1472
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-15 00:04:27 +1000

    Add live debugger views

--- Chibo/Chibo/Views/DebugView.xaml.cs
+++ Chibo/Chibo/Views/DebugView.xaml.cs
@@ -3,18 +3,46 @@ using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Chibo.Services;
+using Chibo.Views.Debuggers;
 
 namespace Chibo.Views
 {
+    /// <summary>
+    /// The view that lists live debuggers
+    /// </summary>
 	public partial class DebugView : ContentPage
 	{
+        /// <summary>
+        /// Gets or sets the debug items.
+        /// </summary>
+        /// <value>The debug items.</value>
 		public List<LiveDebugger> DebugItems { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Chibo.Views.DebugView"/> class.
+        /// </summary>
 		public DebugView()
 		{
 			InitializeComponent();
 
-			DebugItems = LiveDebuggerRegister.GetDebuggers();
-			DebugControlItems.ItemsSource = DebugItems;          
+            // get all the live debuggers
+            DebugItems = LiveDebuggerRegister.GetDebuggers();
+			DebugControlItems.ItemsSource = DebugItems;
+
+			DebugControlItems.ItemSelected += async (sender, e) =>
+			{
+                // quirk of xamarin forms, this fixes some errors
+                if (e.SelectedItem == null)
+                    return;
+
+                // navigation to the debuggerview
+                await Navigation.PushAsync(new DebuggerView() { Debugger = (LiveDebugger)e.SelectedItem });
+
+                // deselect it to unlock the view
+                ((ListView)sender).SelectedItem = null;
+			};
 		}
-	}
+
+    }
 }
---
+++ Chibo/Chibo/Views/Debuggers/DubuggerView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Chibo.LiveDebuggers;
+using Xamarin.Forms;
+
+namespace Chibo.Views.Debuggers
+{
+    /// <summary>
+    /// The view containing the live debugger
+    /// </summary>
+    public partial class DebuggerView : ContentPage
+    {
+        /// <summary>
+        /// Gets or sets the live debugger.
+        /// </summary>
+        /// <value>The debugger.</value>
+		public LiveDebugger Debugger { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Chibo.Views.Debug.DebuggerView"/> class.
+        /// </summary>
+        public DebuggerView()
+        {
+            InitializeComponent();
+
+            // when button is clicked, run the debugger and output into label
+            RunDebuggerButton.Clicked += (sender, ea) => {
+                DebugOutput.Text = Debugger.Execute();
+            };
+
+        }
+
+    }
+}
