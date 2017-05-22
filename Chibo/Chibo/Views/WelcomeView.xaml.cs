31801a6c411f2c5e00fefc830bba06eec7d2209a
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-22 23:40:38 +1000

    Add welcome view to prevent app crashing when state accessed before load

---
+++ Chibo/Chibo/Views/WelcomeView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Chibo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chibo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomeView : ContentPage
+    {
+        public WelcomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void ContentPage_Appearing(object sender, EventArgs e)
+        {
+            PageService.ChangeView(new DashboardView(), "Dashboard");
+        }
+    }
+}
