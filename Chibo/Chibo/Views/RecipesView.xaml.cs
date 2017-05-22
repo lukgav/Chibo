c0e69d1c8572672f4e6873d18d2d85ae5eadf92f
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-22 23:05:08 +1000

    Add code commenting for RecipesView

--- Chibo/Chibo/Views/RecipesView.xaml.cs
+++ Chibo/Chibo/Views/RecipesView.xaml.cs
@@ -6,8 +6,14 @@ using Xamarin.Forms;
 
 namespace Chibo.Views
 {
+    /// <summary>
+    /// View containing a list of all the recipes
+    /// </summary>
     public partial class RecipesView : ContentPage
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Chibo.Views.RecipesView"/> class.
+        /// </summary>
         public RecipesView()
         {
             InitializeComponent();
@@ -15,10 +21,14 @@ namespace Chibo.Views
             // get recipes
             // todo: replace with db
             List<Recipe> recipes = new List<Recipe>();
+
+            // todo: remove this 
             recipes.Add(new Recipe("Test Recipe", new string[] { "Do something", "something else" }, new string[] { "test" }));
+
+            // assign the recipes list element with the recipes
             RecipesList.ItemsSource = recipes;
 
-            // show/hide the label
+            // show/hide the 'You have no recipes' label
             NoRecipesLabel.IsVisible = recipes.Count == 0;
         }
 
@@ -29,8 +39,11 @@ namespace Chibo.Views
         /// <param name="e"></param>
         private async void RecipesList_ItemTapped(object sender, ItemTappedEventArgs e)
         {
+            // get the tapped item
             Recipe selectedItem = (Recipe)(e.Item);
+            // set the selected item on the list to null so it dosen't prevent future selections
             RecipesList.SelectedItem = null;
+            // change to the view recipe page
             await Navigation.PushAsync(new ViewRecipeView(selectedItem));
         }
     }
