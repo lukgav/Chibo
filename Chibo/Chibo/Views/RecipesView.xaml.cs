20ccecfef69dc1f93d7e4b39b41f675a0389712a
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-22 23:02:06 +1000

    Add functionality to recipes view

--- Chibo/Chibo/Views/RecipesView.xaml.cs
+++ Chibo/Chibo/Views/RecipesView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chibo.Models;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
@@ -10,6 +11,27 @@ namespace Chibo.Views
         public RecipesView()
         {
             InitializeComponent();
+
+            // get recipes
+            // todo: replace with db
+            List<Recipe> recipes = new List<Recipe>();
+            recipes.Add(new Recipe("Test Recipe", new string[] { "Do something", "something else" }, new string[] { "test" }));
+            RecipesList.ItemsSource = recipes;
+
+            // show/hide the label
+            NoRecipesLabel.IsVisible = recipes.Count == 0;
+        }
+
+        /// <summary>
+        /// Triggered when a item is selected
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void RecipesList_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Recipe selectedItem = (Recipe)(e.Item);
+            RecipesList.SelectedItem = null;
+            await Navigation.PushAsync(new ViewRecipeView(selectedItem));
         }
     }
 }
---
+++ Chibo/Chibo/Views/ViewRecipeView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Chibo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Chibo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewRecipeView : ContentPage
+    {
+        public Recipe Recipe { get; set; }
+
+        public ViewRecipeView(Recipe recipe)
+        {
+            InitializeComponent();
+            Recipe = recipe;
+
+            RecipeName.Text = recipe.Name;
+        }
+    }
+}
