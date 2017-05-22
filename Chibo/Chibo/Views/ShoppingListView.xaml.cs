ef7030a939573036fa6721f5cbaa2fa402e5c627
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-23 00:15:34 +1000

    Change label attribute for shopping list

--- Chibo/Chibo/Views/ShoppingListView.xaml.cs
+++ Chibo/Chibo/Views/ShoppingListView.xaml.cs
@@ -1,15 +1,30 @@
-﻿using System;
+﻿using Chibo.Models;
+using System;
 using System.Collections.Generic;
 
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace Chibo.Views
 {
+    /// <summary>
+    /// Shopping list view
+    /// </summary>
+    [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ShoppingListView : ContentPage
-    {
+    {    
         public ShoppingListView()
         {
             InitializeComponent();
+
+            // get the shopping list from the menu method
+            ListIngredients ingredients = (Application.Current as App).Menu.CompileShoppingList();
+
+            // assign it to the list element
+            ShoppingList.ItemsSource = ingredients.Ingredients;
+
+            // set the label accordingly
+            ShoppingListCount.Text = String.Format("You have {0} items on your shopping list.", ingredients.Ingredients.Count);
         }
     }
 }
