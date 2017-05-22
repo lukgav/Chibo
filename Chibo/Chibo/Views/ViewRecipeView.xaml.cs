1985b2e374ead2a9a6430f465e149b2620aeb5c8
Mitchell Williamson <mitchell.williamson97@gmail.com>
2017-05-23 00:18:43 +1000

    Add instructions and ingredients to view recipe

--- Chibo/Chibo/Views/ViewRecipeView.xaml.cs
+++ Chibo/Chibo/Views/ViewRecipeView.xaml.cs
@@ -10,17 +10,40 @@ using Xamarin.Forms.Xaml;
 
 namespace Chibo.Views
 {
+    /// <summary>
+    /// The view to view a specific recipe
+    /// </summary>
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ViewRecipeView : ContentPage
     {
+        /// <summary>
+        /// The recipe being viewed
+        /// </summary>
         public Recipe Recipe { get; set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Chibo.Views.ViewRecipeView"/> class.
+        /// </summary>
+        /// <param name="recipe"></param>
         public ViewRecipeView(Recipe recipe)
         {
             InitializeComponent();
+            // assign the recipe
             Recipe = recipe;
 
+            // set the name label
             RecipeName.Text = recipe.Name;
+
+            // iterate through the instructions, generating an ordered list
+            int i = 0;
+            foreach(string instruction in recipe.Instruction)
+            {
+                i++;
+                InstructionsLabel.Text += String.Format("{0}: {1}\n", i, instruction);
+            }
+
+            // assign the ingredients list the recipe's ingredients
+            IngredientsList.ItemsSource = recipe.Ingredients.Ingredients;
         }
     }
 }
